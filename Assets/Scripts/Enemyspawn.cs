f1fe2f5992af47ad185b786a9dab3b9981ae5c7b
balake-fusta <elytra2008@gmail.com>
2026-07-23 20:28:37 +0930

    SPAWN update

--- Assets/Scripts/Enemyspawn.cs
+++ Assets/Scripts/Enemyspawn.cs
@@ -7,26 +7,25 @@ public class Enemyspawn : MonoBehaviour
     public GameObject enemyPrefab;
     public float spawnRadiusX = 5f;
     public float spawnRadiusZ = 6f;
-    public bool spawnEnabled = true;
     public float spawnTimeInterval = 1f;
     public float NumOfESpawned ;
     public float MaxNumOfE = 50f;
-  
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        if (spawnEnabled)
-        { 
-        Vector3 randomSpawnPoint= new Vector3(Random.Range(-spawnRadiusX, spawnRadiusX), 0f, Random.Range(-spawnRadiusZ, spawnRadiusZ));
-        Instantiate(enemyPrefab, randomSpawnPoint, Quaternion.identity);
-            NumOfESpawned++;
-        }
-
-        if (NumOfESpawned == MaxNumOfE)
-        {
-            spawnEnabled = false;
-        }
+        StartCoroutine(SpawnEnemy());
     }
+    IEnumerator SpawnEnemy()
+    {
+            yield return new WaitForSeconds(spawnTimeInterval);
+            Vector3 randomSpawnPoint = new Vector3(Random.Range(-spawnRadiusX, spawnRadiusX), 0f, Random.Range(-spawnRadiusZ, spawnRadiusZ));
+            Instantiate(enemyPrefab, randomSpawnPoint, Quaternion.identity);
+            NumOfESpawned++;
+            if (NumOfESpawned != MaxNumOfE)
+            {
+                StartCoroutine(SpawnEnemy());
+            }
 
+    }
 }
